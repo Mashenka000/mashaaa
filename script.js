6b09bf6d5544e12f5c3250990278748730fb48a9
Mashenka000 <120265672+Mashenka000@users.noreply.github.com>
2022-12-10 17:41:13 +0200

    mash

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿login= prompt ("Enter login: ");
+pass= prompt ("Enter pass");
+var name = "admin";
+var password = "pass";
+
+if (login == name && pass == password)
+{
+  document.write('Welcome, ' +login)
+}
+else if (login == name && pass != password)
+{
+    document.write('Password is wrong!')
+}
+else if (login != name)
+{
+    document.write('Login is wrong!')
+}
